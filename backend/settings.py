c475113958a198ccd4d4010c1e1645af7c1f65b3
Muhammad Faizan Asghar <bsce19021@itu.edu.pk>
2023-04-02 01:38:42 +0500

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-9z%a=^%#*ixk)3b(h+cqpa)cn^6tvy(r4-q970^guw=jdxp_=*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -111,8 +111,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = "/home/faizanasghar43/FYP-React/FYP-Project-main (2)/backend"
+# or, eg,
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
